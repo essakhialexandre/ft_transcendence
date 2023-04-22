cec70f31513649bed698e18dbf61a7590f9d033e
aessakhi <aessakhi@student.42.fr>
2023-04-22 19:39:39 +0200

    Trying to understand migrations properly, should be good, will start working on Users and Channel this week

--- backend/scripts/dev-entrypoint.sh
+++ backend/scripts/dev-entrypoint.sh
@@ -7,4 +7,6 @@ if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- node "$@"
 fi
 
+npx prisma migrate dev
+
 exec "$@"
--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -1,12 +1,16 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class UserDto {
 
+  @IsNumber()
   @IsNotEmpty()
   id: number;
 
+  @IsString()
   @IsNotEmpty()
-  name: string;
+  username: string;
 
+  @IsString()
+  @IsNotEmpty()
   avatar: string;
 }
